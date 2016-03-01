45b8facd0a821d18c42582cfabc36de58b7c697a
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-03-01 15:23:33 +0100

    Multi threading so I can start WS-server as well as HTTP server

--- twidder/runserver.py
+++ twidder/runserver.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
 from gevent.wsgi import WSGIServer
+from geventwebsocket import WebSocketServer, Resource
 from twidder import app
+from multiprocessing import Process
+from twidder.websocket import InactiveEnabler
 
-http_server = WSGIServer(('', 5000), app)
-http_server.serve_forever()
+def http_server(port):
+    http_server = WSGIServer(('', port), app)
+    http_server.serve_forever()
+
+# ------------
+
+def ws_server(port):
+    WebSocketServer(('', port),Resource({'/': InactiveEnabler})).serve_forever()
+
+# ------------
+
+http = Process(target=http_server, args=(5000,))
+wsocket = Process(target=ws_server, args=(8000,))
+
+http.start()
+wsocket.start()
+
+http.join()
+wsocket.join()
